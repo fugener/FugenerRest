5cae438ea1cd925b94a90c7a363e61ab535ea05a
Heiqilin <heiqilinjun@gmail.com>
2018-03-02 05:47:39 -0800

    remove spring dynamo config

--- FugenerRest/src/main/java/com/fugener/rest/FugenerRestApplication.java
+++ FugenerRest/src/main/java/com/fugener/rest/FugenerRestApplication.java
@@ -2,10 +2,8 @@ package com.fugener.rest;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.fugener.rest"})
 public class FugenerRestApplication {
     
 	public static void main(String[] args) {
--- FugenerRest/src/main/java/com/fugener/rest/dynamo/repository/UserRepository.java
+++ FugenerRest/src/main/java/com/fugener/rest/dynamo/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.fugener.rest.dynamo.repository;
 
 import com.fugener.rest.dynamo.model.User;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.CrudRepository;
+import com.fugener.rest.exception.DependencyException;
+import com.fugener.rest.exception.InvalidArgumentException;
 
 import java.util.List;
 
-@EnableScan
-public interface UserRepository extends CrudRepository<User, String> {
-    //List<User> findById(String id);
+public interface UserRepository {
+
+    public User saveUser(final User user) throws DependencyException, InvalidArgumentException;
+
+    public List<User> getUsersByIds(final List<String> userIdList) throws DependencyException, InvalidArgumentException;
 }
